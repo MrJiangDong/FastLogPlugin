27bcc5949aed43d42739464d401b63b58bb92d46
MyJiangDong <675499552@qq.com>
2022-09-20 11:23:35 +0800

    添加了示例代码

--- app/src/main/java/com/jyc/fast/log/MyApplication.kt
+++ app/src/main/java/com/jyc/fast/log/MyApplication.kt
@@ -2,6 +2,11 @@ package com.jyc.fast.log
 
 import android.app.Application
 import com.google.gson.Gson
+import com.jyc.library.fast.log.FastLog
+import com.jyc.library.fast.log.FastLogConfig
+import com.jyc.library.fast.log.FastLogManager
+import com.jyc.library.fast.log.printer.FastConsolePrinter
+import com.jyc.library.fast.log.printer.FastFilePrinter
 
 
 /// @author jyc
@@ -14,33 +19,33 @@ class MyApplication : Application() {
         initLog()
     }
 
-    private fun  initLog(){
-//        FastLogManager.init(
-//            object : FastLogConfig() {
-//
-//                override fun injectJsonParser(): JsonParser {
-//                    return JsonParser { src -> Gson().toJson(src) }
-//                }
-//
-//                override fun getGlobalTag(): String {
-//                    return "MyApplication"
-//                }
-//
-//                override fun enable(): Boolean {
-//                    return true
-//                }
-//            },
-//
-//            FastConsolePrinter(),
-//            FastFilePrinter.getInstance(
-//                FastFilePrinter.getDiskCachePath(applicationContext), 0
-//            )
-//        )
-//
-//        FastLog.a(
-//            FastFilePrinter.getInstance(
-//                FastFilePrinter.getDiskCachePath(applicationContext), 0
-//            )
-//        )
+    private fun initLog() {
+        FastLogManager.init(
+            object : FastLogConfig() {
+
+                override fun injectJsonParser(): JsonParser {
+                    return JsonParser { src -> Gson().toJson(src) }
+                }
+
+                override fun getGlobalTag(): String {
+                    return "MyApplication"
+                }
+
+                override fun enable(): Boolean {
+                    return true
+                }
+            },
+
+            FastConsolePrinter(),
+            FastFilePrinter.getInstance(
+                FastFilePrinter.getDiskCachePath(applicationContext), 0
+            )
+        )
+
+        FastLog.a(
+            FastFilePrinter.getInstance(
+                FastFilePrinter.getDiskCachePath(applicationContext), 0
+            )
+        )
     }
 }
